77f0320c50bdaf85335d9f13b7698285592c967f
arslan <arslanashraf7864@gmail.com>
2022-09-18 14:12:10 +0500

    changing some ui

--- lib/models/cart_data_class.dart
+++ lib/models/cart_data_class.dart
@@ -8,7 +8,7 @@ class Data{
 List<Data> items = [
   Data(ImageUrl: 'https://www.nestle-cereals.com/my/sites/g/files/fawtmp381/files/styles/scale_992/public/d7/koko-crunch_1.png?itok=oaUQvETS',
       Text: 'Nestle Coco Krunch Duo\n(Kids Pack)'),
-  Data(ImageUrl: 'https://nagadmart.com/wp-content/uploads/2021/03/Fresh-Refined-Sugar-600x600.png',
+  Data(ImageUrl: 'https://www.esiopa.com/public/uploads/all/WZRFZ7rBHzgPtuOhy6XDpz3kXE6wQY4ld7GbWhmG.jpg',
       Text: 'Fresh Refined Sugar'),
   Data(ImageUrl: 'https://www.bazarmama.xyz/wp-content/uploads/2021/05/D2D1756E.png',
       Text: 'Ispahani Mirzapure Tea\n(best tea bag)'),
